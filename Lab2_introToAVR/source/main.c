6454b354abc4d665e951ef0c1cfe27d46ea594d1
Paul Nguyen <pnguy086@ucr.edu>
2020-04-13 11:07:54 -0700

    exercise 1

--- Lab2_introToAVR/source/main.c
+++ Lab2_introToAVR/source/main.c
@@ -15,7 +15,7 @@
  int main(void) {
    DDRA = 0x00; PORTA = 0xFF; // Configure port A's 8 pins as inputs
  	 DDRB = 0xFF; PORTB = 0x00; // Configure port B's 8 pins as outputs, initialize to 0s
- 	 //unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
+ 	 unsigned char tmpB = 0x00; // Temporary variable to hold the value of B
  	 unsigned char tmpA = 0x00; // Temporary variable to hold the value of A
    while(1) {
  		   // 1) Read input
