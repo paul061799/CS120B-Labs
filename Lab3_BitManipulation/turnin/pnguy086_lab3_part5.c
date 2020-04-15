d9725f8cdce4e57be839df2e1c223b9da6f91f78
Paul Nguyen <pnguy086@wch136-33.cs.ucr.edu>
2020-04-15 02:04:48 -0700

    Completed part 5

---
+++ Lab3_BitManipulation/turnin/pnguy086_lab3_part5.c
@@ -0,0 +1,34 @@
+/*	Author: pnguy086
+ *  Partner(s) Name:
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRD = 0x00; PORTD = 0xFF;
+    DDRB = 0xFE; PORTB = 0x01; //set PB0 to input and PB7...PB1 as output
+    /* Insert your solution below */
+    unsigned char tmpB = 0;
+    unsigned short weight = 0;
+    while (1) {
+        tmpB = PINB & 0x01;
+        weight = (PIND << 1) | tmpB;
+
+        if(weight >= 70){
+          tmpB = tmpB | 0x04;
+        } else if(weight > 5) {
+          tmpB = tmpB | 0x02;
+        }
+        PORTB = tmpB;
+    }
+    return 1;
+}
