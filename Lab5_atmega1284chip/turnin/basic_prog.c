a19e774bf93f773eb40a7216b598e8561933051d
pnguy086 <pnguy086@ucr.edu>
2020-04-22 10:24:15 -0700

    completed basic program

---
+++ Lab5_atmega1284chip/turnin/basic_prog.c
@@ -0,0 +1,23 @@
+/*	Author: pnguy086
+ *  Partner(s) Name:
+ *	Lab Section:
+ *	Assignment: Lab #  Exercise #
+ *	Exercise Description: [optional - include for your own benefit]
+ *
+ *	I acknowledge all content contained herein, excluding template or example
+ *	code, is my own original work.
+ */
+#include <avr/io.h>
+#ifdef _SIMULATE_
+#include "simAVRHeader.h"
+#endif
+
+int main(void) {
+    /* Insert DDR and PORT initializations */
+    DDRB = 0xFF; PORTB = 0x00; //configure port B's 8 pins as outputs
+    /* Insert your solution below */
+    while (1) {
+        PORTB = 0x0F; //B = 0000 1111
+    }
+    return 1;
+}
