9235320a94e9275113dc48b891a797b3e0e2498f
Paul Nguyen <pnguy086@ucr.edu>
2020-04-22 10:29:07 -0700

    basic led input

--- Lab5_atmega1284chip/source/main.c
+++ Lab5_atmega1284chip/source/main.c
@@ -14,10 +14,21 @@
 
 int main(void) {
     /* Insert DDR and PORT initializations */
+    DDRA = 0x00; PORTA = 0xFF; //onfigure PORTA as input, initialize to 1s
     DDRB = 0xFF; PORTB = 0x00; //configure port B's 8 pins as outputs
+    unsigned char led = 0x00;
+    unsigned char button = 0x00;
     /* Insert your solution below */
     while (1) {
-        PORTB = 0x0F; //B = 0000 1111
+        //if PA is 1, set PB1PB0=01, else  =10
+        // 1) Read inputs
+        button = ~PINA & 0x01; //Button connected to A0
+        if(button){
+            led = (led & 0xFC) | 0x01;//set B to 0x01
+        } else {
+            led = (led & 0xFC) | 0x02;//set B to 0x02
+        }
+        PORTB = led;
     }
     return 1;
 }
